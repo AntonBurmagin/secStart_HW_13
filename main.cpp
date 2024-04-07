06f6dc4f606777c7816447f9e2eb107c66e794b4
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-07 13:29:58 +0300

    first commit: solve 1st task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-
+    // 13.6.1
+    /*
+    int size = 0;
+    std::cout << "Input vector size:\n" ;
+    while (size < 1) {
+        std::cin >> size;
+    }
+    std::vector<int> vec;
+    std::cout << "Input numbers:\n";
+    int element;
+    for (int i = 0; i < size; i++) {
+        std::cin >> element;
+        vec.push_back(element);
+    }
+    std::cout << "Input number to delete:\n";
+    std::cin >> element;
     
+    int k = vec.size() - 1;
+    for (; vec[k] == element && k >= 0; k--) {}
+
+    for (int i = k - 1; i >= 0; i--) {
+        if (vec[i] == element) {
+            for (int j = i; j != k; j++) {
+                std::swap(vec[j], vec[j+1]);
+            }
+            k--;
+        }
+    }
+    
+    for (int k = vec.size() - 1; k >= 0 && vec[k] == element; k--) {
+        vec.pop_back();
+    }
+
+    std::cout << "Result: ";
+    for (int i=0 ; i<vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
+    */
+   
+
 }
